7062a92436903bbaf8157700551006de831d558b
Rakkefet Ifrach <rakkafaprog@gmail.com>
2018-06-04 19:58:51 +0300

    player pause and play works

--- scripts/app.js
+++ scripts/app.js
@@ -10,7 +10,7 @@
 
     // set event for view control:
     viewControl.setEvents({
-      playPlaylist: function(e) {alert("play")},
+      playPlaylist: function(e) {openPlayer($(this).data("id"))},
       editPlaylist: editPlaylistClick,
       deletePlaylist: function(e) {
         modalControl.deleteConfirmation($(this).data("id"));
@@ -26,6 +26,8 @@
     });
 
     modalControl.init();
+
+    
 
   }
 
@@ -54,6 +56,85 @@
     })
   }
 
+  $("#pausePlayer").click(e=>togglePlaying());
+
+  function togglePlaying() {
+    if ($("#pausePlayer").hasClass("glyphicon-play"))
+      $("#player audio")[0].play();
+    else
+      $("#player audio")[0].pause();
+  }
+
+  function openPlayer (playlistId) {
+    data.getSongs(playlistId, d => {
+      console.log(d);
+      $("#songsList .name").text(data.playlistArray[playlistId].name);
+      $("#player .image-cropper img").attr("src",data.playlistArray[playlistId].image );
+      $("#songsList ol").html(songsHtml(d.songs));
+      $("#songsList ol li:first").addClass("current");
+      $("#player").show();
+      changeAudio(d.songs[0].url, d.songs[0].name);
+      $("#songsList li").click(e=>{
+        if ($(e.target).hasClass("current")) {
+          togglePlaying();
+        } else {
+          $(".current").removeClass("current");
+          $(e.target).addClass("current");
+          changeAudio($(e.target).data("url"),$(e.target).text());
+          
+        }
+        
+      });
+    });
+  }
+
+  function songsHtml(songs) {
+    let tempHtml = "";
+    $.each(songs, (index, song)=>{
+      tempHtml += `<li data-url=${song.url}>${song.name}</li>`;
+    });
+    return tempHtml;
+  }
+
+  function changeAudio(url, title) {
+    //console.log("changeing audio " + url);
+    if (url) {
+      $(document).attr("title", title);
+      var audio = $("#player audio");   
+      audio.trigger("pause");   
+      $("#player source").attr("src", url);
+      /****************/
+      audio[0].pause();//.catch(err=>console.log(err));
+      
+      audio[0].load();//.catch(err=>console.log(err));//suspends and restores all audio element
+  
+      //audio[0].play(); //changed based on Sprachprofi's comment below
+      audio[0].oncanplaythrough = audio[0].play();//.catch(err=>console.error("player error"));
+      /****************/
+      audio[0].onended = ()=>{
+        let nxt = $(".current").next();
+        $(".current").removeClass("current");
+        let newurl = nxt.addClass("current").data("url");
+        changeAudio(newurl, nxt.text());
+      };
+      
+    } else {
+      $("#player .image-cropper").removeClass("playing");
+      $("#player i").addClass("glyphicon-pause").removeClass("glyphicon-play");
+    }
+  }
+
+  $("#player audio")[0].addEventListener("play",()=>{
+    $("#player .image-cropper").addClass("playing");
+    $("#player i").addClass("glyphicon-pause").removeClass("glyphicon-play");
+  });
+  $("#player audio")[0].addEventListener("pause",()=>{
+    $("#player .image-cropper").removeClass("playing");
+    $("#player i").addClass("glyphicon-play").removeClass("glyphicon-pause");
+  });
+
+
+  
 
 
 
@@ -61,7 +142,6 @@
 
 
 })();
-
 
 
 
